8f54b093660cba8c97345500f094aa38965be0d3
Aidan Collar <300088221@fvtc.edu>
2024-01-30 19:48:30 -0600

    added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("YOU STINK")
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const greeting=document.querySelector('h')
+greeting.textContent="sdfasdfs"
+
